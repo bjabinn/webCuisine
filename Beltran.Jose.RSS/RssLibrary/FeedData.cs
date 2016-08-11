1440965b13d012c411e829d6cee990848a8aed22
bjabinnn <bjabinnn@gmail.com>
2016-08-11 17:34:57 +0200

    RssLibrary (ready) 2 empty projects (web + WPF)

---
+++ Beltran.Jose.RSS/RssLibrary/FeedData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RssLibrary
+{
+    public class Channel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTimeOffset PubDate { get; set; }
+
+        private List<FeedItem> _Items = new List<FeedItem>();
+        public List<FeedItem> Items
+        {
+            get
+            {
+                return _Items;
+            }
+        }
+    }
+
+    public class FeedItem
+    {
+        private DateTime _pubDate;
+
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Content { get; set; }
+        public string PubDate
+        {
+            get { return _pubDate.ToString("dd-MMM-yyyy HH:mm"); }
+            set { _pubDate = Convert.ToDateTime(value); }
+        }
+
+        public Uri Link { get; set; }
+    }
+
+
+}
---
+++ Beltran.Jose.RSS/RssLibrary/RssLibrary.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Xml;
+using RssLibrary;
+
+namespace RssLibrary{
+    public class RssFeader
+    {
+        #region variables
+        #endregion
+
+        #region Constructor
+        public RssFeader()
+        {
+        }
+
+        public RssFeader(string urlRss)
+        {
+            UrlRs = urlRss;
+        }
+        #endregion 
+
+        #region properties
+        public string UrlRs { get; set; }
+        #endregion
+
+        #region methods
+        public Channel GetWholeChannel()
+        {
+            var feed = SyndicationFeed.Load(XmlReader.Create(UrlRs));
+            
+            try
+            {
+                Channel feedData = null;
+                if (feed != null)
+                {
+                    feedData = new Channel
+                    {
+                        Title = feed.Title.Text,
+                        Description = feed.Description.Text,
+                        PubDate = feed.LastUpdatedTime
+                    };
+
+                    foreach (var item in feed.Items)
+                    {
+                        var newFeed = new FeedItem();
+                        newFeed.Title = item.Title.Text;
+                        newFeed.PubDate = item.PublishDate.DateTime.ToString("G");
+                        if (item.Authors.Any())
+                        {
+                            newFeed.Author = item.Authors[0].Name;
+                        }
+                        newFeed.Content = item.Summary.Text;
+                        if (item.Links.Any())
+                        {
+                            newFeed.Link = item.Links[0].Uri;    
+                        }                        
+                        feedData.Items.Add(newFeed);
+                    }
+                }
+
+                return feedData;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            } 
+        }
+
+        public Channel GetWholeChannelAsync()
+        {
+            return null;
+        }
+
+
+        #endregion
+    }
+}
