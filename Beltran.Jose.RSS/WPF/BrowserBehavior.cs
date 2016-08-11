792f1459ec3117053581c85290d8957dc2fac5ae
bjabinnn <bjabinnn@gmail.com>
2016-08-11 18:59:05 +0200

    WPF - BackgroundWorker + Styles + WebBrowser (binding)

---
+++ Beltran.Jose.RSS/WPF/BrowserBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WPF
+{
+    public static class WebBrowserUtility
+    {
+        public static readonly DependencyProperty HtmlProperty = DependencyProperty.RegisterAttached(
+        "Html", typeof(string), typeof(WebBrowserUtility), new FrameworkPropertyMetadata(OnHtmlChanged));
+
+        [AttachedPropertyBrowsableForType(typeof(WebBrowser))]
+        public static string GetHtml(WebBrowser browser)
+        {
+            return (string)browser.GetValue(HtmlProperty);
+        }
+
+        public static void SetHtml(WebBrowser browser, string value)
+        {
+            browser.SetValue(HtmlProperty, value);
+        }
+
+        static void OnHtmlChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
+        {
+            var browser = dependencyObject as WebBrowser;
+            if (browser != null)
+            {
+                browser.NavigateToString(e.NewValue.ToString());
+            }
+        }
+
+    }
+}
--- Beltran.Jose.RSS/WPF/MainWindow.xaml.cs
+++ Beltran.Jose.RSS/WPF/MainWindow.xaml.cs
@@ -1,28 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using RssLibrary;
 
 namespace WPF
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private Channel _ch;
+
+        private void btnUpdateRSS_Click(object sender, RoutedEventArgs e)
+        {
+            BtnUpdateRss.Content = "Getting Feeds...";
+
+            var worker = new BackgroundWorker { WorkerReportsProgress = true };
+            worker.DoWork += worker_DoWork;
+            worker.RunWorkerCompleted += worker_RunWorkerCompleted;
+            worker.RunWorkerAsync();
+        }
+
+        void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            GridFeeds.ItemsSource = _ch.Items;
+            WebBrowser1.DataContext = _ch.Items;
+            BtnUpdateRss.Content = "Get Feeds of Channel9";
+        }
+
+        void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            _ch = new RssFeader("https://channel9.msdn.com/feeds/rss").GetWholeChannel();
+        }
     }
+
 }
